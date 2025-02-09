01d33d5078317214b2e0a2ea1008d2a634031cba
k-gilvary <kennygilvary82@gmail.com>
2025-02-09 18:27:22 -0500

    loading sequence

---
+++ script.js
@@ -0,0 +1,22 @@
+docBody = document.querySelector("body");
+mainContent = document.getElementById('main-content');
+blackScrn = document.createElement("div");
+blackScrn.classList.add("loading");
+blackScrnTxt = document.createElement("h1");
+blackScrnTxt.classList.add('loading-text', 'loading-text-in');
+blackScrnTxt.textContent = "Loading ...";
+blackScrn.appendChild(blackScrnTxt);
+docBody.insertBefore(blackScrn, docBody.firstChild);
+blackScrnTxt.addEventListener('animationend', () => {
+    blackScrn.remove();
+    mainContent.classList.remove('hidden');
+})
+
+const welcomeFade = () => {
+
+    blackScrnTxt.textContent = 'Welcome.';
+
+    
+}
+
+setTimeout(welcomeFade, 6000)
