34c57d2c072a507ee57c3f7ffc5ab2ba73444656
k-gilvary <kennygilvary82@gmail.com>
2025-02-09 21:44:21 -0500

    device width breakpoints

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
+// get elements
 const docBody = document.querySelector("body");
 const mainContent = document.getElementById('main-content');
-const blackScrn = document.createElement("div");
-const blackScrnTxt = document.createElement("h1");
 
-
+// plays intro sequence
 const intro = () => {
+    // creates black screen
+    const blackScrn = document.createElement("div");
     blackScrn.classList.add("loading");
 
+    //creates black screen text
+    const blackScrnTxt = document.createElement("h1");
     blackScrnTxt.classList.add('loading-text', 'loading-text-in');
     blackScrnTxt.textContent = "Loading ...";
-    blackScrn.appendChild(blackScrnTxt);
 
+    // inserts into page
+    blackScrn.appendChild(blackScrnTxt);
     docBody.insertBefore(blackScrn, docBody.firstChild);
 
+    // remove black screen after animations end
     blackScrnTxt.addEventListener('animationend', () => {
         blackScrn.remove();
         mainContent.classList.remove('d-none');
     })
 
+    // switches loading to welcome
     setTimeout(() => blackScrnTxt.textContent = 'Welcome.', 4000)
 }
 
+// hides any active tabs and shows clicked
 const openTab = (e, tabName) => {
     // hide all tabs
     let tabContent = document.getElementsByClassName('tab-content');
@@ -45,4 +52,4 @@ const d = new Date();
 let year = d.getFullYear();
 copyYear.textContent = year;
 
-// intro();
+intro();
