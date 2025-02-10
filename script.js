f3267b9f21a3b416ac409183b4883d6fcd8e0370
k-gilvary <kennygilvary82@gmail.com>
2025-02-09 21:09:49 -0500

    tabs & complete mobile view

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-docBody = document.querySelector("body");
-mainContent = document.getElementById('main-content');
-blackScrn = document.createElement("div");
-blackScrn.classList.add("loading");
-blackScrnTxt = document.createElement("h1");
-blackScrnTxt.classList.add('loading-text', 'loading-text-in');
-blackScrnTxt.textContent = "Loading ...";
-blackScrn.appendChild(blackScrnTxt);
-docBody.insertBefore(blackScrn, docBody.firstChild);
-blackScrnTxt.addEventListener('animationend', () => {
-    blackScrn.remove();
-    mainContent.classList.remove('d-none');
-})
+const docBody = document.querySelector("body");
+const mainContent = document.getElementById('main-content');
+const blackScrn = document.createElement("div");
+const blackScrnTxt = document.createElement("h1");
 
-const welcomeFade = () => {
 
-    blackScrnTxt.textContent = 'Welcome.';
+const intro = () => {
+    blackScrn.classList.add("loading");
 
-    
+    blackScrnTxt.classList.add('loading-text', 'loading-text-in');
+    blackScrnTxt.textContent = "Loading ...";
+    blackScrn.appendChild(blackScrnTxt);
+
+    docBody.insertBefore(blackScrn, docBody.firstChild);
+
+    blackScrnTxt.addEventListener('animationend', () => {
+        blackScrn.remove();
+        mainContent.classList.remove('d-none');
+    })
+
+    setTimeout(() => blackScrnTxt.textContent = 'Welcome.', 4000)
 }
 
-setTimeout(welcomeFade, 4000)
+const openTab = (e, tabName) => {
+    // hide all tabs
+    let tabContent = document.getElementsByClassName('tab-content');
+    for(let i=0; i<tabContent.length; i++) {
+        tabContent[i].style.display = 'none';
+    }
+
+    // deactivate tab links
+    let tabLinks = document.getElementsByClassName('tab-link');
+    for(let i=0; i<tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(' active', '')
+    }
+
+    // make clicked tab active
+    document.getElementById(tabName).style.display = 'block';
+    e.currentTarget.className += ' active'
+}
+
+// copyright year
+const copyYear = document.getElementById('copy-year');
+const d = new Date();
+let year = d.getFullYear();
+copyYear.textContent = year;
+
+// intro();
